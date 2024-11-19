b994c2a361bb1f14b52ba890b8ca7f5d6b0d4487
Javier Torres Hernandez <javierth2016@gmail.com>
2024-11-19 13:58:45 +0000

    Completar descripciones en source y cambiar promos para una base previa

--- models/staging/sql_server_dbo/stg_sql_server_dbo__promos.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__promos.sql
@@ -1,32 +1,12 @@
-WITH src_promos AS (
+WITH base_promos AS (
     SELECT * 
-    FROM {{ source('sql_server_dbo', 'promos') }}
-    ),
-
-renamed_casted AS (
-    SELECT
-          promo_id as promo_id
-        , promo_id as promo_desc
-        , CASE WHEN discount = null THEN 0
-            ELSE ABS(discount)  
-            END as discount          
-        , status
-        , _fivetran_deleted AS is_deleted
-        , convert_timezone('UTC',_fivetran_synced) as date_load_utc
-    FROM src_promos
-    union all
-    select  'sin_promo'
-            ,'sin_promo'
-            , 0
-            , 'inactive'
-            , null
-            , null
-)
-
-SELECT {{ dbt_utils.generate_surrogate_key(['promo_id']) }} as promo_id
-        , promo_id as promo_description
+    FROM {{ ref('base_sql_server_dbo__promos') }}
+    )
+    
+SELECT promo_id
+        , promo_description
         , discount
-        ,{{ dbt_utils.generate_surrogate_key(['status']) }} as status_id
-        ,is_deleted
-        ,date_load_utc
-FROM renamed_casted
+        , status_id
+        , is_deleted
+        , date_load_utc
+FROM base_promos
--- models/staging/sql_server_dbo/stg_sql_server_dbo__status.sql
+++ models/staging/sql_server_dbo/stg_sql_server_dbo__status.sql
@@ -1,15 +1,14 @@
--- cambiar la base para un unknown status
-
 WITH src_promos AS (
     SELECT * 
-    FROM {{ source('sql_server_dbo', 'promos') }}
+    FROM {{ ref('base_sql_server_dbo__promos') }}
     ),
 
 renamed_casted AS (
-    SELECT DISTINCT status
+    SELECT DISTINCT status_id
+        ,status_desc
     FROM src_promos
     )
 
-SELECT {{ dbt_utils.generate_surrogate_key(['status']) }} as status_id
-        ,status as status_desc
+SELECT status_id
+        ,status_desc
 FROM renamed_casted
